2a8cba56a2f5bb7bce7693874aac7f359f5c0f60
Shehan Tis <shehanproductions@ymail.com>
2013-06-21 17:15:42 +0600

    update

--- inc.topNaviBar.php
+++ inc.topNaviBar.php
@@ -12,89 +12,17 @@
 				</a>
 				<div class="nav-collapse navbar-responsive-collapse collapse">
 					<ul class="nav top-main-navigation">
-						<li class="active dropdown">
-							<a href="index.php" class="dropdown-toggle" data-toggle="dropdown">home</a>
-							<ul class="dropdown-menu">
-								<li><a href="index-2.html">Default store home</a></li>
-								<li><a href="promo_home.html">Default promo home</a></li>
-								<li><a href="store_home_1.html">Store home 1</a></li>
-								<li><a href="store_home_2.html">Store home 2</a></li>
-								<li><a href="promo_home_1.html">Promo home 1</a></li>
-								<li><a href="promo_home_2.html">Promo home 2</a></li>
-							</ul>
-						</li>
+						<li class="active"><a href="index.php" class="dropdown-toggle" data-toggle="dropdown">home</a></li>
 						<li><a href="about-us.html">About us</a></li>
 						<li class="dropdown">
-							<a href="#" class="dropdown-toggle" data-toggle="dropdown">Features</a>
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown">Sights</a>
 							<ul class="dropdown-menu">
-								<li class="empty">HTML5 & CSS3</li>
-								<li class="empty">Responsive Design</li>
-								<li class="empty">Retina Ready</li>
-								<li class="empty">Touchscreen events</li>
-								<li class="empty">eCommerce&nbsp;built-in-styles</li>
-								<li class="empty">Fully customizeble</li>
-								<li class="dropdown">
-									<a href="#" class="dropdown-toggle" data-toggle="dropdown">6 Home Page styles</a>
-									<ul class="dropdown-menu">
-										<li><a href="index-2.html">Home Page 1</a></li>
-										<li><a href="promo_home.html">Home Page 2</a></li>
-										<li><a href="store_home_1.html">Home Page 3</a></li>
-										<li><a href="store_home_2.html">Home Page 4</a></li>
-										<li><a href="promo_home_1.html">Home Page 5</a></li>
-										<li><a href="promo_home_2.html">Home Page 6</a></li>
-									</ul>
-								</li>
-								<li class="dropdown">
-									<a href="#" class="dropdown-toggle" data-toggle="dropdown">9 Header variants</a>
-									<ul class="dropdown-menu">
-										<li><a href="header-one.html">Header 1</a></li>
-										<li><a href="header-two.html">Header 2</a></li>
-										<li><a href="header-third.html">Header 3</a></li>
-										<li><a href="header-four.html">Header 4</a></li>
-										<li><a href="header-five.html">Header 5</a></li>
-										<li><a href="header-six.html">Header 6</a></li>
-										<li><a href="header-seven.html">Header 7</a></li>
-										<li><a href="header-eight.html">Header 8</a></li>
-										<li><a href="header-nine.html">Header 9</a></li>
-									</ul>
-								</li>
-								<li class="dropdown">
-									<a href="#" class="dropdown-toggle" data-toggle="dropdown">4 Footer variants</a>
-									<ul class="dropdown-menu">
-										<li><a href="footer-1.html">Footer Simple</a></li>
-										<li><a href="footer-2.html">Footer Simple + Newslatter signup</a></li>
-										<li><a href="footer-3.html">Footer Simple (inverced)</a></li>
-										<li><a href="footer-4.html">Footer Simple + Newslatter signup (inverced)</a></li>
-									</ul>
-								</li>
-								<li class="dropdown">
-									<a href="#" class="dropdown-toggle" data-toggle="dropdown">Newsletters</a>
-									<ul class="dropdown-menu">
-										<li><a href="newsletter-big-intro.html">Newsletter Big Intro</a></li>
-										<li><a href="newsletter-big-portfolio.html">Newsletter Big Portfolio</a></li>
-										<li><a href="newsletter-columns.html">Newsletter Columns</a></li>
-										<li><a href="newsletter-info.html">Newsletter Info</a></li>
-										<li><a href="newsletter-plan.html">Newsletter Plan</a></li>
-										<li><a href="newsletter-portfolio.html">Newsletter Portfolio</a></li>
-										<li><a href="newsletter-product-list.html">Newsletter Product List</a></li>
-										<li><a href="newsletter-story.html">Newsletter Story</a></li>
-									</ul>
-								</li>
-								<li class="empty">Custom Backgrounds</li>
-								<li class="empty">Unlimited Colors</li>
-								<li class="empty">Unlimited Sidebars</li>
-								<li class="empty">Unlimited Patterns</li>
-								<li class="empty">Google Fonts</li>
-								<li class="empty">Bootstrap Compatible</li>
-								<li class="empty">Dedicated Support</li>
-								<li class="empty">Extended Docs</li>
-								<li class="empty">Clean & Valid Code</li>
-								<li class="empty">Over 50 HTML files</li>
-								<li class="empty">PSD Files Included</li>
+								<li class="empty">All</li>
+								<li class="empty">Other types of locations comes over here</li>
 							</ul>
 						</li>
 						<li class="dropdown">
-							<a href="#" class="dropdown-toggle" data-toggle="dropdown">Pages</a>
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown">Accomadation</a>
 							<ul class="dropdown-menu">
 								<li><a href="full-width-page.html">Full Width</a></li>
 								<li><a href="left-sidebar.html">Left SideBar</a></li>
@@ -163,7 +91,7 @@
 							</ul>
 						</li>
 						<li class="dropdown">
-							<a href="#" class="dropdown-toggle" data-toggle="dropdown">Shop</a>
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown">Services</a>
 							<ul class="dropdown-menu">
 								<li><a href="catalog-grid.html">Catalog (Grid View)</a></li>
 								<li><a href="catalog-grid-2.html">Catalog (Grid View 2)</a></li>
@@ -177,25 +105,13 @@
 							</ul>
 						</li>
 						<li class="dropdown">
-							<a href="#" class="dropdown-toggle" data-toggle="dropdown">Portfolio</a>
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown">Gallery</a>
 							<ul class="dropdown-menu">
 								<li><a href="portfolio-1.html">1 column</a></li>
 								<li><a href="portfolio-2.html">2 column</a></li>
 								<li><a href="portfolio-3.html">3 column</a></li>
 								<li><a href="portfolio-4.html">4 column</a></li>
 								<li><a href="single-project.html">Single Project</a></li>
-							</ul>
-						</li>
-						<li class="dropdown">
-							<a href="#" class="dropdown-toggle" data-toggle="dropdown">Styles</a>
-							<ul class="dropdown-menu">
-								<li><a href="typography.html">Typography</a></li>
-								<li><a href="tables-styles.html">Tables</a></li>
-								<li><a href="forms-styles.html">Forms</a></li>
-								<li><a href="button-designs.html">Buttons</a></li>
-								<li><a href="tabs-designs.html">Tabs</a></li>
-								<li><a href="tooltip-styles.html">Tooltip</a></li>
-								<li><a href="accordions-designs.html">Accordions</a></li>
 							</ul>
 						</li>
 					</ul>
