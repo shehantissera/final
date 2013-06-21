562f0b10db72fc6329ca23983d798ddf6cdb24d5
Shehan Tis <shehanproductions@ymail.com>
2013-06-21 18:10:35 +0600

    update

--- inc.accountStat.php
+++ inc.accountStat.php
@@ -1,7 +1,7 @@
 <?php ?>
 <div class="second-top-box">
             <div class="container-box">
-                <span class="welcome-msg pull-left">Welcome visitor you can <a href="login.html">login</a> or <a href="#">create an account</a>.</span>
+                <span class="welcome-msg pull-left">Welcome visitor you can <a href="login.html">login</a> or <a href="create_account.php">create an account</a>.</span>
                 <ul class="pull-right">
                     <li><a href="#">My Account</a></li>
                     <li><a href="#">My Places</a></li>
